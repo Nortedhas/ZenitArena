788a158d428421f890e3139829f38fd650a64bea
Shtainmets <nortedhas@gmail.com>
2019-12-18 18:29:52 +0300

    change background loading view

--- app/src/main/java/com/ageone/zenit/Modules/Registration/rows/RegistrationTextInputViewHolder.kt
+++ app/src/main/java/com/ageone/zenit/Modules/Registration/rows/RegistrationTextInputViewHolder.kt
@@ -12,20 +12,11 @@ import yummypets.com.stevia.*
 class RegistrationTextInputViewHolder(val constraintLayout: ConstraintLayout) :
     BaseViewHolder(constraintLayout) {
 
-    val phoneTextView by lazy {
-        val textView = BaseTextView()
-        textView.textColor = Color.parseColor("#089988")
-        textView.textSize = 16F
-        textView.setBackgroundColor(Color.TRANSPARENT)
-        textView
-    }
-
     val textInputL by lazy {
         val textInput = BaseTextInputLayout()
 
-        textInput.boxStrokeColor = Color.parseColor("#C1C1C1")
+        textInput.boxStrokeColor = Color.parseColor("#CBCBCB")
         textInput.setInactiveUnderlineColor(Color.rgb(193, 193, 193))
-
         textInput.editText?.let { editText ->
             editText.textColor = Color.parseColor("#000000")
             editText.textSize = 20F
@@ -45,9 +36,6 @@ fun RegistrationTextInputViewHolder.renderUI() {
         textInputL
     )
 
-     textInputL
-         .constrainTopToBottomOf(phoneTextView)
-         .fillHorizontally(14)
 }
 
 fun RegistrationTextInputViewHolder.initialize(hint: String, type: InputEditTextType) {
